a35447788ac4c4d59b20423fb4fad377f8889aec
Giacomo Sfratato <giacomo.sfratato@student.univaq.it>
2019-04-03 11:40:59 +0200

    annullo modifica classe Veicolo

--- src/classi/Veicolo.java
+++ src/classi/Veicolo.java
@@ -9,7 +9,7 @@ private String classeAmbientale;
 private int anno;
 private int assi;
 private int peso;
-private int altezza = 0;
+private int altezza;
 
 public Veicolo(String modello, String marca, String targa, String classeAmbientale, int anno, int assi, int peso,
 		int altezza) {
